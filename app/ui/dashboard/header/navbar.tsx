a80a955ebfef21943b9f344387c0d6a5fc05a955
Franco <frfantusatti@protonmail.com>
2025-07-04 15:33:50 -0300

    adding styles for menuItem

--- app/ui/dashboard/header/navbar.tsx
+++ app/ui/dashboard/header/navbar.tsx
@@ -88,13 +88,28 @@ export default async function Navbar({
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md  py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
               >
                 <MenuItem>
-                  <Link href="/products/profile">Your Profile</Link>
+                  <Link
+                    className="block px-4 py-2 text-sm text-base data-focus:bg-base-100 data-focus:outline-hidden"
+                    href="/products/profile"
+                  >
+                    Your Profile
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <Link href="/products/profile/cart">My Cart</Link>
+                  <Link
+                    className="block px-4 py-2 text-sm text-base-700 data-focus:bg-base-100 data-focus:outline-hidden"
+                    href="/products/profile/cart"
+                  >
+                    My Cart
+                  </Link>
                 </MenuItem>
                 <MenuItem>
-                  <Link href="/products/profile/wishes">My Wishlist</Link>
+                  <Link
+                    className="block px-4 py-2 text-sm text-base-700 data-focus:bg-base-100 data-focus:outline-hidden"
+                    href="/products/profile/wishes"
+                  >
+                    My Wishlist
+                  </Link>
                 </MenuItem>
               </MenuItems>
             </Menu>
